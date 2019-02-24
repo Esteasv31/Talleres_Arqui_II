a927a105df5087f1f31af684c90911b5aeed6d99
essanabria <essanabria@ic-itcr.ac.cr>
2019-02-24 01:23:54 -0600

    SAXPY function added

--- Taller 2/SAXPY/serial.c
+++ Taller 2/SAXPY/serial.c
@@ -2,3 +2,12 @@
 // Created by vidarr on 2/24/19.
 //
 
+void saxpy(int n, float a, float * restrict x, float * restrict y)
+{
+    for (int i = 0; i < n; ++i)
+        y[i] = a*x[i] + y[i];
+}
+
+
+// Perform SAXPY on 1M elements
+saxpy(1<<20, 2.0, x, y);
